fbdeb4f76fe7f5ccb7ea8c5602de1c7c8078a12e
nsmeds <niilosmeds@gmail.com>
2017-02-25 10:29:09 -0800

    removed stray character

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,7 @@ class About extends Component {
                 <p>Riverbed is a poetry magazine of the online realm. We are currently hard at work building our first issue. To be notified when it is published, enter your email below. If you'd like to contribute work for publication, <a href="#">email us</a>. Preferred format is pdf, or paste your work in the body of the email.</p>
 
                 <form className="form">
-                    <input type='email' placeholder='Your email' name='email' />>
+                    <input type='email' placeholder='Your email' name='email' />
                     <button className="button">Submit</button>
                 </form>
             </div>
