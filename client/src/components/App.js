7947a90f21e8af00d59064091d36c6ee1e939415
nsmeds <niilosmeds@gmail.com>
2017-02-27 15:46:20 -0800

    restructured components; moved GET request to Home component to address routing issue

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,37 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import axios from 'axios';
-import Contributors from './Contributors';
-import MainContent from './MainContent';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      posts: []
-    };
-  }
-
-  componentDidMount() {
-    this.getPosts();
-  }
-
-  getPosts = () => {
-    axios.get(`http://localhost:3001/api/posts`)
-      .then(response => {
-        this.setState({
-          posts: response.data
-        });
-      })
-      .catch(error => {
-        console.log('Error: could not GET posts. ', error);
-      })
-  }
-
-
   render() {
-    console.log(this.state.posts);
     return ( 
       <div id="header">
         <Link to="index"><h1>Riverbed</h1></Link>
@@ -44,8 +16,6 @@ class App extends Component {
                 <li><Link to="/admin">Admin</Link></li>
             </ul>
         </nav>
-        <Contributors data={this.state.posts} />
-        <MainContent data={this.state.posts} />
         { this.props.children }
       </div>
     );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Contributors from './Contributors';
+import MainContent from './MainContent';
 
 class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts = () => {
+    axios.get(`http://localhost:3001/api/posts`)
+      .then(response => {
+        this.setState({
+          posts: response.data
+        });
+      })
+      .catch(error => {
+        console.log('Error: could not GET posts. ', error);
+      })
+  }
+
     render() {
         return (
             <div>
-                {/*<Contributors data={this.state.posts} />*/}
+                <Contributors data={this.state.posts} />
+                <MainContent data={this.state.posts} />
                 {this.props.children}
             </div>
         );
