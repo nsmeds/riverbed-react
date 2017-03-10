8657958195592f6482c26fe5dd62c401db5f783e
nsmeds <niilosmeds@gmail.com>
2017-03-09 16:56:15 -0800

    Going to have to use componentDidMount and make a server call in Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Contributors from './Contributors';
 import MainContent from './MainContent';
 
+
+// maybe just make this a class and use componentDidMount to grab the current issue every time, I guess.
+
 const Home = props => {
 
   console.log('props from Home', props);
