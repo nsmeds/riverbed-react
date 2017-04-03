2c054c156e50bcf84355d1e94f2340f38bc24466
nsmeds <niilosmeds@gmail.com>
2017-04-03 16:39:24 -0700

    minor styling

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -10,17 +10,20 @@ const Signin = props => {
     }
 
     return (
-        <form className="form" onSubmit={props.handleLogin}>
-            <label>
-                <span>Username: </span>
-                <input type="text" name="username" value={newUser.username} onChange={props.handleInputChange} />
-            </label>
-            <label>
-                <span>Password: </span>
-                <input type="password" name="password" value={newUser.password} onChange={props.handleInputChange} />
-            </label>
-            <button className="button">Log In</button>
-        </form>
+        <div className="menu-item">
+            <h2>Sign In</h2>
+            <form className="form signin-form" onSubmit={props.handleLogin}>
+                <label>
+                    <span>Username: </span>
+                    <input type="text" name="username" value={newUser.username} onChange={props.handleInputChange} />
+                </label>
+                <label>
+                    <span>Password: </span>
+                    <input type="password" name="password" value={newUser.password} onChange={props.handleInputChange} />
+                </label>
+                <button className="button">Log In</button>
+            </form>
+        </div>
     )
 }
 
